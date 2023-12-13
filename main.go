79008c2d2f45547cc64945ce0c9753c00589a326
sotaheavymetal21 <euro246seeker@gmail.com>
2023-12-13 20:52:56 +0900

    configlist

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/myproject/todo_go/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
